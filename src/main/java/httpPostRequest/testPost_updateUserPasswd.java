52ef9fd8535fc3364fb81d4d3acd6b826aaa75cf
cnliuy <cnliuy@163.com>
2015-10-21 18:13:29 +0800

    10.21

---
+++ src/main/java/httpPostRequest/testPost_updateUserPasswd.java
@@ -0,0 +1,51 @@
+package httpPostRequest;
+
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+
+/**
+ * 全版本的Post 
+ * 测试 更新用户  密码
+ * */
+public class testPost_updateUserPasswd {
+
+	public static void main(String[] args) throws Exception {
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		
+		String url = "http://localhost:8080/lr/api/v1/usertools/updateuserPasswd?username=fab8cf43a14d4f90aab28d31ce1aa11b&digest=e60e633cd564e24bcc4bcf91b1c3d7ccb9966d9a" ;
+		HttpPost httpPost = new HttpPost(url);
+		List <NameValuePair> nvps = new ArrayList <NameValuePair>();
+		nvps.add(new BasicNameValuePair("passwd", "admin")); //关键值 必须有  非空键值  填完并不做验证 但是需要填写			
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+		CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+		try {			
+		    System.out.println(response2.getStatusLine());		    
+		    HttpEntity entity2 = response2.getEntity();
+		    String response2txt=EntityUtils.toString(entity2);
+		    System.out.println(response2txt);
+
+		} finally {
+		    response2.close();
+		}
+
+	}
+
+}
